ac50416d797e1fc201605b43b5525d582e7a0cb9
inanna2 <sally.a.jerjees@gmail.com>
2026-01-18 23:12:43 +0000

    msg_pythag

--- partTwo.py
+++ partTwo.py
@@ -1,9 +1,13 @@
 import math  
 
 def main():
-#TO DO  
+    A = int(input("enter A:"))
+    B = int(input("enter B:"))
+    result=pythag(A,B)
+    print("The result is :", result)
 
 def pythag(A,B):
-#TO DO  
+    result_1=A**2 + B**2
+    return math.isqrt(result_1)
 
 main()
